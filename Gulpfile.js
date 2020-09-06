abe9351049ea666540e14f73a1901bd87942c9b5
aceade <philiprowlands90@gmail.com>
2020-09-06 12:45:48 +0100

    #4 adding uglify task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,8 @@
 const jshint = require('gulp-jshint');
 const gulp   = require('gulp');
 const { series } = require('gulp');
+const uglify = require('gulp-uglify');
+const pipeline = require('readable-stream').pipeline;
 
 // define a JSHint task
 function lint() {
@@ -12,6 +14,15 @@ function lint() {
     .pipe(jshint.reporter('fail'));
 }
 
+function compress() {
+    return pipeline(
+        gulp.src('src/goodCode.js'),
+        uglify(),
+        gulp.dest('dist')
+  );
+}
+
 // export it as a module
 exports.lint = lint;
-exports.default = series(lint);
+exports.compress = compress;
+exports.default = series(lint, compress);
